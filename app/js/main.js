6ef365d4335657359798e05576f7aeca8bbb26d2
Leo <leochan2017@gmail.com>
2018-05-14 11:17:02 +0800

    v.1.1.0版本

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,8 @@
-window.open('http://product.landray.com.cn/km/workhours/km_workhours_main/kmWorkhoursMain.do?method=addDialog')
+document.getElementById('getWorkReportInfo').onclick = function() {
+  open('http://product.landray.com.cn/km/workhours/km_workhours_calendar/worcalendar.jsp')
+}
+
+
+document.getElementById('writeWorkReport').onclick = function() {
+  open('http://product.landray.com.cn/km/workhours/km_workhours_main/kmWorkhoursMain.do?method=addDialog')
+}
